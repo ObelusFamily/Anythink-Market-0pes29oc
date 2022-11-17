a9c4f0f3fde853a25e4e0a7717873ca08cc4607b
geneo924 <genemoore924@gmail.com>
2022-11-17 15:58:03 -0500

    (added) - fix to api

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -172,7 +172,6 @@ router.get("/:item", auth.optional, function(req, res, next) {
 
 // returns item by title
 router.get('/', function(req, res, next) {
-  
   Item.find().distinct(req.body.item.title).then(function(title){
     return res.json({title: title});
   }).catch(next);
